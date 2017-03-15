32be471e5823577335e831d7b84b0204c2d3cdc8
Achmad Arifin <achmad.arifin.mta@gmail.com>
2017-03-15 11:48:23 +0700

    Add files via upload

---
+++ template/main.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Simple Booking</title>
+		<link rel="stylesheet" type="text/css" media="screen" href="../asset/css/style.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="../asset/css/jquery-ui.css" />
+		<script type="text/javascript" src="../asset/js/jquery-1.7.2.min.js"></script>
+		<script type="text/javascript" src="../asset/js/jquery-ui.min.js"></script>		
+	<head>	
+	<body>
+		<?php echo $templateContent ?>
+	</body>	
+</html>
